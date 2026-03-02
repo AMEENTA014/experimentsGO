94fb9ecd7ceeb015cc21e34b33ff8f8ee73273cf
AMEENTA014 <ameenta41@gmail.com>
2026-03-02 20:59:20 +0530

    created a conceptualRAM

---
+++ go.mod
@@ -0,0 +1,3 @@
+module microcore
+
+go 1.25.7
---
+++ main.go
@@ -0,0 +1,24 @@
+package main 
+import("fmt")
+type PhysicalMemory struct{
+    data []byte
+}
+func NewPhysicalMemory(size int32 )   *PhysicalMemory {
+            return &PhysicalMemory{
+              data: make([]byte , size),
+	    }
+}
+func (m * PhysicalMemory)  Read(addr uint32) byte {
+        return m.data[addr]
+}
+
+func (m * PhysicalMemory)  Write(addr uint32 , value byte)  {
+        m.data[addr] =  value 
+}
+func main(){
+ mem := NewPhysicalMemory(1024)
+ mem.Write(10 , 42)
+ value := mem.Read(10)
+ fmt.Println("value at address 10 " , value )
+
+ }
